d1114321c425f1ae3cfec46ec216717ec7b3a8ed
Uncle Hub <habahaba959@gmail.com>
2017-04-28 11:20:11 +0300

    всё поменял работает

--- src/Shapes/Doo.java
+++ src/Shapes/Doo.java
@@ -5,9 +5,9 @@ public class Doo {
 
     public static void main(String[] args) {
 
-        Square a1 = new Square(5.6, "заштрихованный");
-        Triangle a2 = new Triangle(2.8, "синий");
-        Circle a3 = new Circle(3.8, "в фиолетовую крапинку");
+        Shapes a1 = new Square(5.6, "заштрихованный");
+        Shapes a2 = new Triangle(2.8, "синий");
+        Shapes a3 = new Circle(3.8, "в фиолетовую крапинку");
         a1.drawShape();
         a2.drawShape();
         a3.drawShape();
